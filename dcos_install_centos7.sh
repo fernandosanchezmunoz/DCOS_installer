093390bee8fb555736a8f2cd90bf136a156243f3
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2016-11-07 19:28:41 -0500

    correct systemd unit files

--- dcos_install_centos7.sh
+++ dcos_install_centos7.sh
@@ -49,7 +49,7 @@ ELK_CA_NAME=ca.crt
 ELK_HOSTNAME=$BOOTSTRAP_IP
 ELK_PORT=9200
 FILEBEAT_JOURNALCTL_CONFIG=/etc/filebeat/filebeat_journald.yml
-FILEBEAT_JOURNALCTL_SERVICE=/etc/systemd/system/dcos-journalctl-filebeat.service
+FILEBEAT_JOURNALCTL_SERVICE=/etc/systemd/system/multi-user.target.wants/dcos-journalctl-filebeat.service
 
 #pretty colours
 RED='\033[0;31m'
@@ -616,55 +616,60 @@ After=filebeat.service
 [Service]
 Restart=always
 RestartSec=5
-ExecStart=/bin/bash /usr/bin/journalctl --since="now" --no-tail -f \
-        -u dcos-3dt.service                \                            
-        -u dcos-3dt.socket                 \
-        -u dcos-adminrouter-reload.service \
-        -u dcos-adminrouter-reload.timer   \
-        -u dcos-adminrouter.service        \
-        -u dcos-bouncer.service            \
-        -u dcos-ca.service                 \
-        -u dcos-cfn-signal.service         \
-        -u dcos-cosmos.service             \
-        -u dcos-download.service           \
-        -u dcos-epmd.service               \
-        -u dcos-exhibitor.service          \
-        -u dcos-gen-resolvconf.service     \
-        -u dcos-gen-resolvconf.timer       \
-        -u dcos-history.service            \
-        -u dcos-link-env.service           \
-        -u dcos-logrotate-master.timer     \
-        -u dcos-marathon.service           \
-        -u dcos-mesos-dns.service          \
-        -u dcos-mesos-master.service       \
-        -u dcos-metronome.service          \
-        -u dcos-minuteman.service          \
-        -u dcos-navstar.service            \
-        -u dcos-networking_api.service     \
-        -u dcos-secrets.service            \
-        -u dcos-setup.service              \
-        -u dcos-signal.service             \
-        -u dcos-signal.timer               \
-        -u dcos-spartan-watchdog.service   \
-        -u dcos-spartan-watchdog.timer     \
-        -u dcos-spartan.service            \
-        -u dcos-vault.service              \
-        -u dcos-logrotate-master.service   \  
-  > /var/log/dcos/dcos.log
+ExecStart=/bin/sh -c '/usr/bin/journalctl --no-tail -f \
+  -u dcos-3dt.service \                            
+  -u dcos-3dt.socket \
+  -u dcos-adminrouter-reload.service \
+  -u dcos-adminrouter-reload.timer   \
+  -u dcos-adminrouter.service        \
+  -u dcos-bouncer.service            \
+  -u dcos-ca.service                 \
+  -u dcos-cfn-signal.service         \
+  -u dcos-cosmos.service             \
+  -u dcos-download.service           \
+  -u dcos-epmd.service               \
+  -u dcos-exhibitor.service          \
+  -u dcos-gen-resolvconf.service     \
+  -u dcos-gen-resolvconf.timer       \
+  -u dcos-history.service            \
+  -u dcos-link-env.service           \
+  -u dcos-logrotate-master.timer     \
+  -u dcos-marathon.service           \
+  -u dcos-mesos-dns.service          \
+  -u dcos-mesos-master.service       \
+  -u dcos-metronome.service          \
+  -u dcos-minuteman.service          \
+  -u dcos-navstar.service            \
+  -u dcos-networking_api.service     \
+  -u dcos-secrets.service            \
+  -u dcos-setup.service              \
+  -u dcos-signal.service             \
+  -u dcos-signal.timer               \
+  -u dcos-spartan-watchdog.service   \
+  -u dcos-spartan-watchdog.timer     \
+  -u dcos-spartan.service            \
+  -u dcos-vault.service              \
+  -u dcos-logrotate-master.service  \  
+  > /var/log/dcos/dcos.log 2>&1'
 ExecStartPre=/usr/bin/journalctl --vacuum-size=10M
 
 [Install]
 WantedBy=multi-user.target
 EOF
-sudo chmod 0755 /etc/systemd/system/$FILEBEAT_JOURNALCTL_SERVICE
 
 else #if not master
 
 echo "** Creating service to parse DC/OS Agent logs into Filebeat ..."
 sudo tee $FILEBEAT_JOURNALCTL_SERVICE<<-EOF 
-#!/bin/bash
+[Unit]
+Description=DCOS journalctl parser to filebeat
+Wants=filebeat.service
+After=filebeat.service
 
-journalctl --since="now" --no-tail -f      \
+[Service]
+Restart=always
+RestartSec=5
+ExecStart=/bin/sh -c '/usr/bin/journalctl --no-tail -f      \
   -u dcos-3dt.service                      \
   -u dcos-logrotate-agent.timer            \
   -u dcos-3dt.socket                       \
@@ -688,17 +693,21 @@ journalctl --since="now" --no-tail -f      \
   -u dcos-link-env.service                 \
   -u dcos-vol-discovery-priv-agent.service \
   -u dcos-logrotate-agent.service          \
-  > /var/log/dcos/dcos.log
+  > /var/log/dcos/dcos.log 2>&1'
 ExecStartPre=/usr/bin/journalctl --vacuum-size=10M
+
+[Install]
+WantedBy=multi-user.target
 EOF
 
 EOF
-sudo chmod 0755 /etc/systemd/system/$FILEBEAT_JOURNALCTL_SERVICE
+
 fi 
 #if role=MASTER
 
 echo "** Installed Filebeat (aka. logstash-forwarder) ... "
 
+sudo chmod 0755 $FILEBEAT_JOURNALCTL_SERVICE
 sudo systemctl daemon-reload
 sudo systemctl start $FILEBEAT_JOURNALCTL_SERVICE
 sudo chkconfig $FILEBEAT_JOURNALCTL_SERVICE on
