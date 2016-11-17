2a48ce9012581ca0fcb107c37aba66631a3e8b44
Fernando Sanchez <fernando@mesosphere.io>
2016-11-16 19:17:13 -0500

    fix releasever for docker repo

--- dcos_install_rh7.sh
+++ dcos_install_rh7.sh
@@ -174,7 +174,7 @@ systemctl stop firewalld &&  systemctl disable firewalld
 sudo cat > /etc/yum.repos.d/docker.repo << 'EOF'
 [dockerrepo]
 name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
 enabled=1
 gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg
