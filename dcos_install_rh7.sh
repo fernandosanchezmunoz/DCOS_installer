d564cd827593c7bcc6b196fdad103c9a0b1a2dc4
Fernando Sanchez <fernando@mesosphere.io>
2016-11-18 16:36:11 -0500

    add install marathon-lb

--- dcos_install_rh7.sh
+++ dcos_install_rh7.sh
@@ -906,6 +906,38 @@ fi #if INSTALL_ELK = true
 ################################################################################################################################
 ################################################################################################################################
 
+#Install Marathon-LB
+####################
+echo "** Installing Marathon-LB on the public node..."
+dcos package install --yes --cli dcos-enterprise-cli
+dcos security org service-accounts keypair -l 4096 marathon-lb-private-key.pem marathon-lb-public-key.pem
+dcos security org service-accounts create -p marathon-lb-public-key.pem -d "dcos_marathon_lb service account" dcos_marathon_lb
+dcos security org service-accounts show dcos_marathon_lb
+sleep 2
+curl -skSL -X PUT -H 'Content-Type: application/json' -d '{"description": "Marathon Services"}' -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:service:marathon:marathon:services:%252F
+curl -skSL -X PUT -H 'Content-Type: application/json' -d '{"description": "Marathon Events"}' -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:service:marathon:marathon:admin:events
+curl -skSL -X PUT -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:service:marathon:marathon:services:%252F/users/dcos_marathon_lb/read
+curl -skSL -X PUT -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:service:marathon:marathon:admin:events/users/dcos_marathon_lb/read
+sleep 2
+dcos security secrets create-sa-secret marathon-lb-private-key.pem dcos_marathon_lb marathon-lb
+dcos security secrets list /
+dcos security secrets get /marathon-lb --json | jq -r .value | jq
+
+# Launch Marathon-LB using the secret and the cert created above
+tee marathon-lb-secret-options.json <<'EOF'
+{
+     "marathon-lb": {
+         "secret_name": "marathon-lb"
+     }
+}
+EOF
+
+dcos package install --options=marathon-lb-secret-options.json --yes marathon-lb
+
+clear
+#open up a tab with the public node:
+MESSAGE=$'Open up a tab pointing to: \n'
+echo -e "${BLUE} $MESSAGE ${RED} http://$PUBLIC:9090/haproxy?stats ${NC}"
 
 #Check that installation finished successfully.
 #################################################################
