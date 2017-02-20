4094c9992da9c19e8735e5d618757e87b27fd744
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2017-02-20 16:26:40 +0100

    fix Os check

--- dcos_install_rh7.sh
+++ dcos_install_rh7.sh
@@ -70,7 +70,7 @@ fi
 
 #make sure we're running on RHEL 7.2
 if [ $(grep "ID=" /etc/os-release | head -n1) != "ID=\"rhel\"" ] || \
-   [[ $(grep "ID=" /etc/os-release | tail -n1) != "VERSION_ID=\"7.2\"" ; then
+   [[ $(grep "ID=" /etc/os-release | tail -n1) != "VERSION_ID=\"7.2\"" ]]; then
    #&& \ $(grep "ID=" /etc/os-release | tail -n1) != "VERSION_ID=\"7.3\"" ]]
   echo "** This installer supports RHEL 7.2 only. Aborting"
   exit
