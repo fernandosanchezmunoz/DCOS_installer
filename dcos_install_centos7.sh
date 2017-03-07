382da971ac0953571b0f3a1df64da4b9ed032133
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-03-07 12:29:18 -0500

    wget to curl -O on node installer

--- dcos_install_centos7.sh
+++ dcos_install_centos7.sh
@@ -504,7 +504,7 @@ setenforce 0
 echo "** Installing dependencies..."
 
 #install jq
-wget http://stedolan.github.io/jq/download/linux64/jq
+curl -O http://stedolan.github.io/jq/download/linux64/jq
 chmod +x ./jq
 yes | cp -rf jq /usr/bin
 
