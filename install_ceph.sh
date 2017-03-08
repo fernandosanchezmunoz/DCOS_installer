1c3b990083809c3390e1e8a00aa451bdaf9aa0f7
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-03-07 19:34:36 -0500

    correct colors

--- install_ceph.sh
+++ install_ceph.sh
@@ -36,7 +36,7 @@ until $(curl --output /dev/null --silent --head --fail http://ceph.mesos:5000); 
 done
 
 echo -e "${NC}Ceph is available at http://PUBLIC-NODE:5000. Please log in and configure Ceph Monitors and OSDs following the instructions in:"
-echo -e "${BLUE}**https://github.com/dcos/examples/tree/master/1.8/ceph#configure-ceph"
+echo -e "${BLUE}**https://github.com/dcos/examples/tree/master/1.8/ceph#configure-ceph${NC}"
 
 #do not continue until Ceph is configured and monitors are reachable
 while true; do
