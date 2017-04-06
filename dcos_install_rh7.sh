4d8c6beb2e5e8b9a1f284c9ebc3f00bdb56af1a2
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2017-04-06 04:48:59 -0400

    Update dcos_install_rh7.sh

--- dcos_install_rh7.sh
+++ dcos_install_rh7.sh
@@ -1070,32 +1070,32 @@ fi #if INSTALL_ELK = true
 #########################################################################
 if [ "$INSTALL_CEPH" == true ]; then 
 
-  curl -sSL https://dl.bintray.com/emccode/rexray/install | sh -s -- stable 0.8.1
+curl -sSL https://dl.bintray.com/emccode/rexray/install | sh -s -- stable 0.8.1
 
-  cat > /etc/rexray/config.yml << EOF
-  rexray:
-  #  loglevel: debug #not needed
-  libstorage:
-    service: rbd
-  EOF
-  #copy to libstorage config
-  cp -f /etc/rexray/config.yml /etc/libstorage/config.yml > /dev/null 2>&1
+cat > /etc/rexray/config.yml << EOF
+rexray:
+#  loglevel: debug #not needed
+libstorage:
+  service: rbd
+EOF
+#copy to libstorage config
+cp -f /etc/rexray/config.yml /etc/libstorage/config.yml > /dev/null 2>&1
 
-  cat > /etc/systemd/system/rexray << EOF
-  [Unit]
-  Description=REX-Ray: A vendor agnostic storage orchestration engine
+cat > /etc/systemd/system/rexray << EOF
+[Unit]
+Description=REX-Ray: A vendor agnostic storage orchestration engine
 
-  [Service]
-  StartLimitInterval=0
-  Restart=always
-  RestartSec=15
-  LimitNOFILE=16384
-  Environment=REXRAY_HOME=/
-  ExecStart=/usr/bin/rexray start -f
-  EOF
+[Service]
+StartLimitInterval=0
+Restart=always
+RestartSec=15
+LimitNOFILE=16384
+Environment=REXRAY_HOME=/
+ExecStart=/usr/bin/rexray start -f
+EOF
 
-  systemctl daemon-reload
-  systemctl start rexray
+systemctl daemon-reload
+systemctl start rexray
 fi
 
 
