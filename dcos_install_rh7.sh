4147929b89d22dd530db0f75178fb7f2c870396d
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2017-04-05 20:21:07 -0400

    add rexray to bootstrap node

--- dcos_install_rh7.sh
+++ dcos_install_rh7.sh
@@ -1066,6 +1066,39 @@ fi #if INSTALL_ELK = true
 ################################################################################################################################
 ################################################################################################################################
 
+#Install rexray on bootstrap node to access docker volumes in the cluster
+#########################################################################
+if [ "$INSTALL_CEPH" == true ]; then 
+
+  curl -sSL https://dl.bintray.com/emccode/rexray/install | sh -s -- stable 0.8.1
+
+  cat > /etc/rexray/config.yml << EOF
+  rexray:
+  #  loglevel: debug #not needed
+  libstorage:
+    service: rbd
+  EOF
+  #copy to libstorage config
+  cp -f /etc/rexray/config.yml /etc/libstorage/config.yml > /dev/null 2>&1
+
+  cat > /etc/systemd/system/rexray << EOF
+  [Unit]
+  Description=REX-Ray: A vendor agnostic storage orchestration engine
+
+  [Service]
+  StartLimitInterval=0
+  Restart=always
+  RestartSec=15
+  LimitNOFILE=16384
+  Environment=REXRAY_HOME=/
+  ExecStart=/usr/bin/rexray start -f
+  EOF
+
+  systemctl daemon-reload
+  systemctl start rexray
+fi
+
+
 #Check that installation finished successfully.
 #################################################################
 sleep 3
